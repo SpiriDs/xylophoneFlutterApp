ffd647b027a484e2ba7d9ef88f9a46124005e49d
DESKTOP-UPBE1VP\Spiri <spirids@live.de>
2019-05-28 16:18:47 +0700

    Optimise App with function for buttons with 2 Args Expanded buttons()

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,18 @@ class XylophoneApp extends StatelessWidget {
     player.play('note$soundNo.wav');
   }
 
+  Expanded buttons({Color color, int soundNo}) {
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: () {
+          playSound(soundNo);
+        },
+        child: null,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -20,69 +32,13 @@ class XylophoneApp extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              Expanded(
-                child: FlatButton(
-                  color: Colors.red,
-                  onPressed: () {
-                    playSound(1);
-                  },
-                  child: null,
-                ),
-              ),
-              Expanded(
-                child: FlatButton(
-                  color: Colors.orange,
-                  onPressed: () {
-                    playSound(2);
-                  },
-                  child: null,
-                ),
-              ),
-              Expanded(
-                child: FlatButton(
-                  color: Colors.yellow,
-                  onPressed: () {
-                    playSound(3);
-                  },
-                  child: null,
-                ),
-              ),
-              Expanded(
-                child: FlatButton(
-                  color: Colors.green,
-                  onPressed: () {
-                    playSound(4);
-                  },
-                  child: null,
-                ),
-              ),
-              Expanded(
-                child: FlatButton(
-                  color: Colors.teal,
-                  onPressed: () {
-                    playSound(5);
-                  },
-                  child: null,
-                ),
-              ),
-              Expanded(
-                child: FlatButton(
-                  color: Colors.blue,
-                  onPressed: () {
-                    playSound(6);
-                  },
-                  child: null,
-                ),
-              ),
-              Expanded(
-                child: FlatButton(
-                  color: Colors.purple,
-                  onPressed: () {
-                    playSound(7);
-                  },
-                  child: null,
-                ),
-              ),
+              buttons(color: Colors.red, soundNo: 1),
+              buttons(color: Colors.orange, soundNo: 2),
+              buttons(color: Colors.yellow, soundNo: 3),
+              buttons(color: Colors.green, soundNo: 4),
+              buttons(color: Colors.teal, soundNo: 5),
+              buttons(color: Colors.blue, soundNo: 6),
+              buttons(color: Colors.purple, soundNo: 7),
             ],
           ),
         ),
